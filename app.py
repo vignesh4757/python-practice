5a2c6517f36e5ec648f9822b8eda642ae2d58899
Vignesh V <113326108+vignesh4757@users.noreply.github.com>
2023-06-23 15:04:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def dessert_order():
+    flavour = st.text_input("What flavour would you like?")
+    dessert_type = st.text_input("What type of dessert would you like?")
+    order_button = st.button("Place Order")
+
+    if order_button:
+        st.write("You have ordered", flavour + "-" + dessert_type)
+
+# Main app
+def main():
+    st.title("Dessert Ordering App")
+    dessert_order()
+
+if __name__ == "__main__":
+    main()
